debdea371931c0bf38c8f0b5583021490f146973
qbuat <quentin.buat@cern.ch>
2016-06-08 19:33:00 +0200

    writer

--- Root/Writer.cxx
+++ Root/Writer.cxx
@@ -13,6 +13,7 @@
 // EDM
 #include "xAODEventInfo/EventInfo.h"
 #include "xAODTau/TauJetContainer.h"
+#include "xAODTau/TauJetAuxContainer.h"
 #include "xAODJet/JetContainer.h"
 #include "xAODMuon/MuonContainer.h"
 #include "xAODEgamma/ElectronContainer.h"
@@ -20,9 +21,6 @@
 #include "xAODMissingET/MissingET.h"
 #include "xAODTruth/TruthParticle.h"
 #include "xAODTruth/TruthParticleContainer.h"
-
-// tools
-#include "TrigDecisionTool/TrigDecisionTool.h"
 
 // Local stuff
 #include "HiggsTauTauXaod/tools.h"
@@ -44,7 +42,7 @@ EL::StatusCode Writer :: setupJob (EL::Job& job)
   job.useXAOD ();
   EL_RETURN_CHECK("setupJob", xAOD::Init("Writer"));
 
-  EL::OutputStream out("hist", "xAOD");
+  EL::OutputStream out("hist");
   job.outputAdd(out);
 
   return EL::StatusCode::SUCCESS;
@@ -87,14 +85,25 @@ EL::StatusCode Writer :: initialize ()
 
 EL::StatusCode Writer :: execute ()
 {
-  const xAOD::EventInfo * ei = 0;
-  EL_RETURN_CHECK("execute", Utils::retrieve(ei, "EventInfo", m_event, m_store));
+  // const xAOD::EventInfo * ei = 0;
+  // EL_RETURN_CHECK("execute", Utils::retrieve(ei, "EventInfo", m_event, m_store));
 
-  m_event->copy("EventInfo");
- // if (not m_event->record(ei, "EventInfo"))
- //    return EL::StatusCode::FAILURE;
 
-  ATH_MSG_INFO("fill event "<< wk()->treeEntry());
+  xAOD::TauJetContainer* taus = 0;
+  EL_RETURN_CHECK("execute", Utils::retrieve(taus, "SelectedTaus", 0, m_store));
+
+  xAOD::TauJetAuxContainer* taus_aux = 0;
+  EL_RETURN_CHECK("execute", Utils::retrieve(taus_aux, "SelectedTausAux.", 0, m_store));
+
+
+  m_event->record(taus, "SelectedTaus");
+  m_event->record(taus_aux, "SelectedTausAux.");
+
+  // m_event->copy("EventInfo");
+  // m_event->copy("SelectedTaus");
+
+  
+  // ATH_MSG_INFO("fill event "<< wk()->treeEntry());
   m_event->fill();
 
   return EL::StatusCode::SUCCESS;
@@ -111,7 +120,7 @@ EL::StatusCode Writer :: postExecute ()
 
 EL::StatusCode Writer :: finalize ()
 {
-  ATH_MSG_INFO("finalize");
+  // ATH_MSG_INFO("finalize");
   TFile *file = wk()->getOutputFile ("hist");
   EL_RETURN_CHECK("finalize", m_event->finishWritingTo( file ));
   return EL::StatusCode::SUCCESS;
